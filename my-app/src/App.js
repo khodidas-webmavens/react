6795592879c0c31dc351a9ecc41db3d01b8229ae
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-01-04 18:47:02 +0530

    update 4 jan

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-center">
+      <header className="App-header text-center d-flex justify-content-center">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+        hello world
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>web mavens</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
