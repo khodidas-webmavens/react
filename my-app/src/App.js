35918fd4e562bb5e641a0f5eb8b18f2923af123c
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-01-04 18:59:33 +0530

    update 4 jan

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,20 +5,31 @@ import './App.css';
 function App() {
   return (
     <div className="App text-center">
-      <header className="App-header text-center d-flex justify-content-center">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-        hello world
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <h1>web mavens</h1>
+      <header className="App-header text-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
     </div>
   );
